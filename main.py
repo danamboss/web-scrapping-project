70ce95eb8344b3379289b9232ac4efd8eaa0377e
danamboss <100099797+danamboss@users.noreply.github.com>
2022-04-25 17:57:23 +0000

    added a for loop to which can be used to get tags

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ html_doc = r.text
 soup = BeautifulSoup(html_doc, features="html5lib")
 prttysp = soup.prettify()
 
-print(prttysp)
+print(soup.title)
+print(soup.get_text)
 
+tags = soup.find_all('a')
 
-"""for link in soup.find_all('a'):
-  print(link.get('href'))"""
+for link in tags:
+  print(link.get('href'))
