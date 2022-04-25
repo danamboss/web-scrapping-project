1ed2fb56d31098e661d33a05affc4da8caaad218
danamboss <100099797+danamboss@users.noreply.github.com>
2022-04-25 17:42:47 +0000

    add prettify to align the html code

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ url = 'https://sharonbandele.me/'
 r = requests.get(url)
 html_doc = r.text
 soup = BeautifulSoup(html_doc, features="html5lib")
-print(soup)
+prttysp = soup.prettify()
 
-for link in soup.find_all('a'):
-  print(link.get('href'))
+print(prttysp)
+
+
+"""for link in soup.find_all('a'):
+  print(link.get('href'))"""
